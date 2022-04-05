e7dc5a17d24bbca303bb42f87c2381a8e3b2514b
ahanafislam <ahanafislam@gmail.com>
2022-04-05 06:54:43 +0600

    Finished Blog Components

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -5,7 +5,7 @@ import { Container } from 'react-bootstrap';
 const NotFound = () => {
     return (
         <Container>
-            <div className='w-50 position-absolute top-50 start-50 translate-middle'>
+            <div className='w-50 me-auto ms-auto'>
                 <img src={page404} alt="Page_Not_Found" className='img-fluid'/>
                 <h1 className='text-center brand-color'>Sorry Page Not Found.</h1>
             </div>
