dc5776a318dc007216f62e4a1a31487544bf55cf
ahanafislam <ahanafislam@gmail.com>
2022-04-05 08:38:01 +0600

    Add Month wise sell chirt in dashboard

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -4,11 +4,11 @@ import { Card, Col, Container, Row } from 'react-bootstrap';
 const Blogs = () => {
     return (
         <Container className='mt-5'>
-        <Row className='g-5'>
+        <Row className='g-4'>
             <Col>
                 <div className='reviewCard position-relative mb-4'>
                     <Card.Body>
-                        <Card.Title className='mt-3'>What is context API?</Card.Title>
+                        <Card.Title className='mt-3 brand-color'>What is context API?</Card.Title>
                         <Card.Text>
                             The main purpose of using context API is to avoid prop-drilling in our application. In react we can easily create components under components. After breaking components into smaller parts we might need some data to work. If we want to access the data we need to pass the data through props from the parent component to the child component. Hare if we use Context API we can easily use the data directly from the grandparent component to child component without any props passed by the parents. This is how we can easily avoid prop-drilling with the help of Context API.
                         </Card.Text>
@@ -18,7 +18,7 @@ const Blogs = () => {
             <Col sm={1} md={6}>
                 <div className='reviewCard position-relative mb-4'>
                     <Card.Body>
-                        <Card.Title className='mt-3'>What is Semantic Tag?</Card.Title>
+                        <Card.Title className='mt-3 brand-color'>What is Semantic Tag?</Card.Title>
                         <Card.Text>
                             In HTML have some tag like &lt;div&gt; and &lt;span&gt; this is not saying about its content. On the other hand &lt;header$&gt;, &lt;footer&gt; and &lt;article&gt; tag describe the purpose of the element and why should we use it. So, this type of tag is calld semantic tag. The semantic tag describes its meaning for the browser and developer. Basically, a Semantic tag is much easier to read. It’s has best accessibility. Semantic tag are not only for easier to read but it’s also SEO friendly.
                         </Card.Text>
--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,26 +1,14 @@
 import React from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
 import useData from '../../hooks/useData';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, BarChart, Bar } from 'recharts';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, BarChart, Bar, AreaChart, Area} from 'recharts';
 
 const Dashboard = () => {
     const [data, setData] = useData();
 
     return (
         <Container className="mt-4">
-            <Row className="g-4">
-                <Col sm={1} md={6}>
-                    <h5 className='text-center brand-color'>Investment VS Revenue</h5>
-                    <LineChart width={500} height={300} data={data} margin={{ top: 5, right: 5, bottom: 5, left: 10 }}>
-                        <XAxis dataKey="month" stroke='#666'/>
-                        <YAxis dataKey="investment" stroke='#666'/>
-                        <CartesianGrid stroke="#999" strokeDasharray="5 5"/>
-                        <Line type="monotone" dataKey="investment" stroke="#F60" />
-                        <Line type="monotone" dataKey="revenue" stroke="#006f95" />
-                        <Tooltip/>
-                        <Legend />
-                    </LineChart>
-                </Col>
+            <Row className="g-5">
                 <Col sm={1} md={6}>
                     <h5 className='text-center brand-color'>Investment VS Revenue</h5>
                     <BarChart width={500} height={300} data={data} margin={{top: 20,right: 30,left: 20,bottom: 5,
@@ -34,6 +22,39 @@ const Dashboard = () => {
                     <Bar dataKey="investment" stackId="a" fill="#f60" />
                     <Bar dataKey="revenue" stackId="a" fill="#006f95" />
                     </BarChart>
+                </Col>
+                <Col sm={1} md={6}>
+                    <h5 className='text-center brand-color'>Month Wise Sell</h5>
+                    <AreaChart
+                    width={500}
+                    height={300}
+                    data={data}
+                    margin={{
+                        top: 10,
+                        right: 30,
+                        left: 0,
+                        bottom: 0,
+                    }}
+                    >
+                    <CartesianGrid strokeDasharray="3 3" stroke='#999'/>
+                    <XAxis dataKey="month" stroke='#666'/>
+                    <YAxis dataKey="sell" stroke='#666'/>
+                    <Tooltip />
+                    <Legend />
+                    <Area type="monotone" dataKey="sell" stroke="#F60" fill="#F60" />
+                    </AreaChart>
+                </Col>
+                <Col sm={1} md={6}>
+                    <h5 className='text-center brand-color'>Investment VS Revenue</h5>
+                    <LineChart width={500} height={300} data={data} margin={{ top: 5, right: 5, bottom: 5, left: 10 }}>
+                        <XAxis dataKey="month" stroke='#666'/>
+                        <YAxis dataKey="investment" stroke='#666'/>
+                        <CartesianGrid stroke="#999" strokeDasharray="5 5"/>
+                        <Line type="monotone" dataKey="investment" stroke="#F60" />
+                        <Line type="monotone" dataKey="revenue" stroke="#006f95" />
+                        <Tooltip/>
+                        <Legend />
+                    </LineChart>
                 </Col>
             </Row>
         </Container>
